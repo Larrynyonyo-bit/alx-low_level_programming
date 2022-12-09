5463d35d9002f06a7405e813831a509a8414d801
Larrynyonyo-bit <nyokwame@gmail.comgit config --global user.email nyokwame@gmail.com>
2022-12-09 05:30:10 -0800

    first first

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
- *  * main- enttry point for program
- *   *
- *    * Return: always return 0
- *     */
+ *main- enttry point for program
+ *
+ *Return: always return 0
+ */
 
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
-
 
