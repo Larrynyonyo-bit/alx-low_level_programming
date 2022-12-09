950c4d327a51c9f9ca562f296a134fbce56c927b
Larrynyonyo-bit <nyokwame@gmail.comgit config --global user.email nyokwame@gmail.com>
2022-12-09 15:55:51 -0800

    first

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ * Return: always return 0 & (Sucess)
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
