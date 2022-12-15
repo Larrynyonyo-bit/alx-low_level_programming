82cf1f4430f0be93d4c706ef6b301e550ba033e9
Larrynyonyo-bit <nyokwame@gmail.comgit config --global user.email nyokwame@gmail.com>
2022-12-14 23:46:12 -0800

    functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,18 @@
 #include "main.h"
 /**
- * main- entry point, print char
- *
- * Return: Always Success
+ * main - print _putchare
+ * Return: means success
  */
-
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char hacker[] = "_putchar";
 
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(hacker[c]);
+	}
+	_putchar('\n');
 	return (0);
 }
-
